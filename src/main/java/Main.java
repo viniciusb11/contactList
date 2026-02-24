5f5bc5057c156514059371598b9986cc830032a0
viniciusb11 <officialvincontact@gmail.com>
2026-02-24 14:18:11 -0300

    added faker to generate test data

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,14 @@
+import com.github.javafaker.Faker;
+
 public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     ContactList myContacts = new ContactList();
     boolean isRunning = true;
+    Faker faker = new Faker();
 
     while (isRunning) {
         System.out.println("What option would you like to choose?");
-        System.out.println("1. Add contact\n2. Remove contact\n3. View contact list\n4. Exit");
+        System.out.println("1. Add contact\n2. Remove contact\n3. View contact list\n4. Generate Test Data\n5. Exit");
         int option = scanner.nextInt();
         scanner.nextLine();
         if (option == 1) {
@@ -33,7 +36,17 @@ public static void main(String[] args) {
             }
         } else if (option == 3) {
             System.out.println(myContacts.contactsList);
-        } else if (option == 4){
+        } else if (option == 4) {
+            for (int i = 0; i < 10; i++) {
+                String name = faker.name().fullName();
+                String phone = faker.phoneNumber().phoneNumber();
+                faker.name().fullName();
+                faker.phoneNumber().phoneNumber();
+                Contact dataTest = new Contact(name, phone);
+                myContacts.contactsList.add(dataTest);
+            }
+            System.out.println("10 random contacts generated succesfully!");
+        } else if (option == 5){
             System.out.println("Bye!");
             isRunning = false;
         } else {
